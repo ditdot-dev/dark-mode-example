98468041dd5ff107f07c2d15ffbd94a203ae70dd
54369630 <54369630+phre1@users.noreply.github.com>
2020-07-27 15:13:13 +0200

    Optimized stars.svg, Stars now visible on light mode

--- app.js
+++ app.js
@@ -5,18 +5,33 @@
 */
 
 function load() {
+  "use strict";
+
   const switcher = document.querySelector("input");
   const title = document.querySelector(".title");
   const logo = document.querySelector(".logo");
 
+  String.prototype.format = function() {
+    let formatted = this;
+    
+    for (let i = 0; i < arguments.length; i++) {
+        let regexp = new RegExp('\\{'+i+'\\}', 'gi');
+        formatted = formatted.replace(regexp, arguments[i]);
+    }
+
+    return formatted;
+  };
+
   function calculatePath() {
     const width = window.innerWidth;
     const height = window.innerHeight;
-    const path = `path("M ${width / 2 - 25}, ${height / 2} m ${
-      -width / 2
-      }, 0 a ${width / 2},${height / 2} 0 1,0 ${width},0 a ${width / 2},${
-      height / 2
-      } 0 1,0 ${-width},0z")`;
+    const path =
+      'path("M {0}, {1} m -{2}, 0 a {2}, {1} 0 1, 0 {3}, 0 a {2}, {1} 0 1,0 -{3}, 0z")'.format(
+        width / 2 - 25,
+        height / 2,
+        width / 2,
+        width
+      );
 
     return path;
   }
@@ -29,11 +44,21 @@ function load() {
   // Toggles the "dark-mode" class based on if the media query matches
   function toggleDarkMode(state) {
     checkToggle(state);
-    document.body.classList.toggle("dark-mode", state);
+
+    const hasClass = document.body.classList.contains("dark-mode");
+    
     if (state) {
+      if (!hasClass) {
+        document.body.classList.add("dark-mode");
+      }
+
       title.textContent = "Dark mode";
       logo.style.offsetPath = calculatePath();
     } else {
+      if (hasClass) {
+        document.body.classList.remove("dark-mode");
+      }
+
       title.textContent = "Light mode";
       logo.style.offsetPath = "none";
     }
@@ -41,17 +66,19 @@ function load() {
 
   // MediaQueryList object
   const useDark = window.matchMedia("(prefers-color-scheme: dark)");
-  let dark = useDark.matches;
+  let darkModeState = useDark.matches;
 
   // Listen for changes in the OS settings
-  useDark.addListener((evt) => toggleDarkMode(evt.matches));
+  useDark.addListener(function(evt) {
+    toggleDarkMode(evt.matches);
+  });
 
   // Initial setting depending on the prefers-color-mode
-  toggleDarkMode(dark);
+  toggleDarkMode(darkModeState);
 
   function switchListener() {
-    dark = !dark;
-    toggleDarkMode(dark);
+    darkModeState = !darkModeState;
+    toggleDarkMode(darkModeState);
   }
 
   // Listen for switch change
